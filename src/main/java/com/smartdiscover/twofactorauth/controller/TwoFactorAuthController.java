267c007b9c2dd4f773c2f32d10e1800bfa379ffc
Anshul BANSAL <anshul.bansal@vivacredit.ro>
2020-10-30 18:47:36 +0200

    Email integration - once user logs in - an email will be sent to the user with the verification link

--- src/main/java/com/smartdiscover/twofactorauth/controller/TwoFactorAuthController.java
+++ src/main/java/com/smartdiscover/twofactorauth/controller/TwoFactorAuthController.java
@@ -6,14 +6,11 @@ import com.smartdiscover.twofactorauth.repository.TwoFactorAuthRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.util.HtmlUtils;
 
 import java.sql.Date;
 import java.util.concurrent.CompletableFuture;
